43f9e561f3a73054b2f3136a6a76bc7f5393b968
cyberstrawberry101 <cyberstrawberry101@gmail.com>
2017-02-10 13:07:24 -0700

    Merge Test

--- BSM/MODULES/ServerManagement.cs
+++ BSM/MODULES/ServerManagement.cs
@@ -32,7 +32,6 @@ namespace Borealis
         //===================================================================================//
         private void btnUpdateServerConfig_Click(object sender, EventArgs e)
         {
-            txtCustomerName.Enabled = false;
             MetroMessageBox.Show(this, "Unfortunately this feature has not been implemented yet.  Please wait for an update to fix this!", "Not Implemented Yet", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
